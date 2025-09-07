61540cd32e342aab83fc07f0e71a89c8ddda033c
Hatim Shakir <hatimshakir9179@gmail.com>
2025-09-08 01:15:13 +0530

    fix: toggle theme minor fix

--- components/theme-switcher.tsx
+++ components/theme-switcher.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import * as React from "react";
-import { Moon, Sun } from "lucide-react";
+import { LaptopMinimal, Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
 
 import { Button } from "@/components/ui/button";
@@ -11,30 +11,35 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { useEventCallback } from "@/hooks/use-event-callback";
+
+const THEMES = ["light", "dark", "system"];
+
+const THEME_VS_ICONS = {
+  light: Sun,
+  dark: Moon,
+  system: LaptopMinimal,
+};
 
 export function ThemeSwitcher() {
-  const { setTheme } = useTheme();
+  const { theme, setTheme } = useTheme();
+  const ThemeIcon = THEME_VS_ICONS[theme as keyof typeof THEME_VS_ICONS];
+
+  const toggleTheme = useEventCallback(() => {
+    const nextTheme =
+      THEMES[(THEMES.indexOf(theme || "light") + 1) % THEMES.length];
+    setTheme(nextTheme);
+  });
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button className="rounded-full" variant="ghost" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
-          System
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <Button
+      className="rounded-full"
+      variant="ghost"
+      size="icon"
+      onClick={toggleTheme}
+    >
+      <ThemeIcon className="size-4" />
+      <span className="sr-only">Toggle theme</span>
+    </Button>
   );
 }
